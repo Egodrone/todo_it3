61b7769e6f6405b37a2363bfc223c7bd3344b1ad
Eddie <eddie.ch.89d@gmail.com>
2021-01-06 17:02:03 +0100

    People class, removing user by id.

--- todo_it2-master/src/main/java/se/lexicon/eddie/data/People.java
+++ todo_it2-master/src/main/java/se/lexicon/eddie/data/People.java
@@ -3,48 +3,83 @@ package se.lexicon.eddie.data;
 
 
 import java.util.Arrays;
-import se.lexicon.eddie.data.PersonSequencer;
 import se.lexicon.eddie.model.Person;
 
 
 
 public class People {
     //Empty array
-    private static Person[] person = new Person[0];
+    private static Person[] personArr = new Person[0];
+
+
 
     //Return the length of the array
     public int size() {
-        return person.length;
+        return personArr.length;
     }
+
+
 
     //Return the array
     public Person[] findAll() {
-        return person;
+        return personArr;
     }
+
+
 
     public Person findById(int personId) {
         int checkSize = size();
         Person searchForPerson = new Person();
 
-        if(checkSize > 0 && checkSize - 1 >= personId){
+        if (checkSize > 0 && checkSize - 1 >= personId) {
             //Person is in the array
-            searchForPerson = person[personId];
+            searchForPerson = personArr[personId];
         }
         return searchForPerson;
     }
 
-    public static Person[] createNewPerson(Person addNewPerson){
+
+
+    public static Person[] createNewPerson(Person addNewPerson) {
         //PersonSequencer sequencer = new PersonSequencer();
         //int nextId = sequencer.nextPersonId();
-        Person[] returnArray = Arrays.copyOf(person, person.length + 1);
+        Person[] returnArray = Arrays.copyOf(personArr, personArr.length + 1);
         returnArray[returnArray.length - 1] = addNewPerson;
         return returnArray;
     }
 
+
+
+    //Remove the person by the id
+    public void removePerson(int persProvId) {
+        int removeIndex = 0;
+        for (int i = 0; i < personArr.length; i++) {
+            if(personArr[i].getPersonId() == persProvId) {
+                //person id is found, no need to iterate further
+                removeIndex = i;
+                break;
+            }
+        }
+
+        // Creating new array to store existing values - person to remove
+        Person[] copyArr = new Person[personArr.length - 1];
+
+        for (int i = 0, j = 0; i < personArr.length; i++) {
+            if (i == removeIndex) {
+                continue;
+            }
+            copyArr[j++] = personArr[i];
+        }
+        personArr = copyArr;
+    }
+
+
+
     public void clear() {
         //reset the array
-        person = new Person[0];
+        personArr = new Person[0];
     }
+
 
 
 }
