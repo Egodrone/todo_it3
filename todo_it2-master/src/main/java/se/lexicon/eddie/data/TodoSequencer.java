b3642caf764758a682c5df30b0aa3e271db7b22c
Eddie <eddie.ch.89d@gmail.com>
2021-01-06 22:23:16 +0100

    TodoSequencer

--- todo_it2-master/src/main/java/se/lexicon/eddie/data/TodoSequencer.java
+++ todo_it2-master/src/main/java/se/lexicon/eddie/data/TodoSequencer.java
@@ -1,12 +1,13 @@
 package se.lexicon.eddie.data;
 
+
+
 public class TodoSequencer {
     private static int personId = 0;
 
-    public static int nextPersonIdTodo(int personId) {
+    public static int nextPersonIdTodo() {
         return personId + 1;
     }
-
 
     static int resetTodo(){
         personId = 0;
