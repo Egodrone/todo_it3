83f5e064d2e2d786461e950660bb13b77e6eb706
Eddie <eddie.ch.89d@gmail.com>
2021-01-10 18:51:37 +0100

    Todoitems class

--- todo_it2-master/src/main/java/se/lexicon/eddie/data/TodoItems.java
+++ todo_it2-master/src/main/java/se/lexicon/eddie/data/TodoItems.java
@@ -2,7 +2,7 @@ package se.lexicon.eddie.data;
 
 
 
-//import se.lexicon.eddie.model.Person;
+import se.lexicon.eddie.model.Person;
 import se.lexicon.eddie.model.Todo;
 import java.util.Arrays;
 
@@ -51,7 +51,127 @@ public class TodoItems {
 
     public void clear() {
         //reset the array
-        todoArr = null;
+        todoArr = new Todo[0];
+    }
+
+
+
+    public Todo[] findByAssignee(int personId) {
+        int tmp = 0;
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getAssignee().getPersonId() == personId) {
+                tmp++;
+            }
+        }
+
+        Todo[] assigneeArr = new Todo[tmp];
+        tmp = 0;
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getAssignee().getPersonId() == personId) {
+                assigneeArr[tmp] = todoArr[i];
+                tmp++;
+            }
+        }
+        return assigneeArr;
+    }
+
+
+
+    public Todo[] findByAssignee(Person assignee) {
+        int tmp = 0;
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getAssignee().getPersonId() == assignee.getPersonId()) {
+                tmp++;
+            }
+        }
+
+        Todo[] assigneeArr = new Todo[tmp];
+        tmp = 0;
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getAssignee().getPersonId() == assignee.getPersonId()) {
+                assigneeArr[tmp] = todoArr[i];
+                tmp++;
+            }
+        }
+        return assigneeArr;
+    }
+
+
+
+    public Todo[] findUnassignedTodoItems() {
+        int tmp = 0;
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getAssignee() == null) {
+                tmp++;
+            }
+        }
+
+        Todo[] assigneeNotSet = new Todo[tmp];
+        tmp = 0;
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getAssignee() == null) {
+                assigneeNotSet[tmp] = todoArr[i];
+                tmp++;
+            }
+        }
+        return assigneeNotSet;
+    }
+
+
+
+    public Todo[] findByDoneStatus(boolean doneStatus) {
+        int done, notDone, a, b;
+        done = notDone = a = b = 0;
+
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getDone() == true) {
+                done++;
+            } else {
+                notDone++;
+            }
+        }
+        Todo[] doneArr = new Todo[done];
+        Todo[] notDoneArr = new Todo[notDone];
+
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getDone() == true && done != 0) {
+                doneArr[a] = todoArr[i];
+                a++;
+                done--;
+            } else {
+                if (notDone != 0) {
+                    notDoneArr[b] = todoArr[i];
+                    b++;
+                    notDone--;
+                }
+            }
+        }
+
+        if (doneStatus == true) {
+            return doneArr;
+        }
+        return notDoneArr;
+    }
+
+
+
+    public void removeTodo(int personId) {
+        int tmp = 0;
+        for (int i = 0; i < todoArr.length; i++) {
+            if (todoArr[i].getTodoId() == personId) {
+                tmp = i;
+                break;
+            }
+        }
+        Todo[] newArr = new Todo[todoArr.length - 1];
+
+        for(int i = 0, j = 0; i < todoArr.length; i++) {
+            if (i == tmp) {
+                continue;
+            }
+            newArr[j++] = todoArr[i];
+        }
+        todoArr = newArr;
     }
 
 }
