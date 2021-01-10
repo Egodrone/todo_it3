69dda5427f46f227d9a035369eef4035f7b286ce
Eddie <eddie.ch.89d@gmail.com>
2021-01-10 18:55:52 +0100

    Todo test with Junit

--- todo_it2-master/src/test/java/se/lexicon/eddie/model/TodoTest.java
+++ todo_it2-master/src/test/java/se/lexicon/eddie/model/TodoTest.java
@@ -2,12 +2,16 @@ package se.lexicon.eddie.model;
 
 
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 
 
 public class TodoTest {
+    private Todo todo1;
+
+
 
     @Test
     public void test_todoBoolean() {
@@ -15,21 +19,43 @@ public class TodoTest {
         assertFalse(todoClass.getDone());
     }
 
+
+
     //test of the default description setter
     @Test
     public void test_getDescription() {
         Todo todoClass = new Todo();
         assertNull(todoClass.getDescription());
+
+        //set description
+        todoClass.setDescription("New description");
+        String expected = "New description";
+        assertEquals(expected, todoClass.getDescription());
     }
 
-    //test of the default id
+
+
     @Test
     public void test_id() {
         int todoIdTest = 0;
-        int expected = 1;
+        int expected = 2;
         Todo todoClass = new Todo(todoIdTest, "test");
         assertEquals(expected,
                 todoClass.getTodoId());
+    }
+
+
+
+    @Test
+    public void test_assignee() {
+        todo1 = new Todo();
+        Person testPerson = new Person("Eddie", "Hello");
+        //System.out.println(testPerson.getFirstName() +" "+ todo1.getAssignee().getFirstName());
+        //System.out.println(testPerson.getFirstName());
+        todo1.setAssignee(testPerson);
+        //System.out.println(todo1.getAssignee().getFirstName());
+        assertEquals(testPerson.getFirstName(), todo1.getAssignee().getFirstName());
+        assertEquals(testPerson.getLastName(), todo1.getAssignee().getLastName());
     }
 
 }
